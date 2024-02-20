029dbc90031f1dffd8d25502f037535f28979cdb
allan <boukhebza.allan@outlook.fr>
2024-02-20 17:23:58 +0100

    fix(scrapper) fix doawnload file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 use std::env;
+use reqwest::header::{ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE, HeaderMap, USER_AGENT};
 use reqwest::Url;
 use scraper::{Html, Selector};
 
@@ -12,41 +13,58 @@ async fn main() {
 
     let mut url_list: HashSet<String> = HashSet::new();
 
-    let default_url = args.get(1).unwrap().clone();
-    start_scrapping(default_url, &mut url_list).await.expect("Error while scrapping");
+    for arg in args.iter().skip(1) {
+        println!("{}", arg);
+        url_list.insert(arg.clone());
+    }
+
+    start_scrapping(&mut url_list).await.expect("Error while scrapping");
 
     // http client
 
 }
 
-async fn start_scrapping(default_url: String, url_list: &mut HashSet<String>) -> Result<(), &'static str> {
-    let result = fetch_url(&default_url).await;
-    println!("List size : {}", &result.len());
-    url_list.extend(result);
+async fn start_scrapping(url_list: &mut HashSet<String>) -> Result<(), &'static str> {
+    let mut headers = HeaderMap::new();
+    headers.insert(USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0".parse().unwrap());
+    headers.insert(ACCEPT_LANGUAGE, "en-US,en;q=0.5".parse().unwrap());
 
+    let mut i = 0;
     while !url_list.is_empty() {
         if let Some(url) = url_list.iter().next().cloned() {
-            let result = fetch_url(&url).await;
-            println!("Found : {}", result.len());
+            let result = fetch_url(&url, &headers).await;
             url_list.extend(result);
             url_list.remove(&url);
         }
+        println!("iter : {}", i);
         println!("List size : {}", url_list.len());
+        println!("List : {:?}\n", url_list.iter().take(5).collect::<Vec<&String>>());
+
+        i = i + 1;
     }
 
     Ok(())
 }
 
-async fn fetch_url(url: &String) -> HashSet<String> {
-    if let Ok(res) = reqwest::get(url).await {
-        if let Ok(body) = res.text().await {
-            if let Ok(urls) = extract_urls(Html::parse_document(&*body)).await {
-                return urls;
+async fn fetch_url(url: &String, header: &HeaderMap) -> HashSet<String> {
+    let client = reqwest::Client::new();
+    if let Ok(res) = client.head(url).headers(header.clone()).send().await {
+        if let Some(content_type) = res.headers().get(reqwest::header::CONTENT_TYPE) {
+            if content_type.to_str().unwrap().contains("text/html") {
+                // Continue seulement si le Content-Type est text/html
+                if let Ok(res) = client.get(url).headers(header.clone()).send().await {
+                    if let Ok(body) = res.text().await {
+                        if let Ok(urls) = extract_urls(Html::parse_document(&*body)).await {
+                            return urls;
+                        }
+                    }
+                }
             }
         }
     }
     HashSet::new()
 }
+
 
 async fn extract_urls(document: Html) -> Result<HashSet<String>, &'static str> {
     let mut urls: HashSet<String> = HashSet::new();
@@ -54,7 +72,6 @@ async fn extract_urls(document: Html) -> Result<HashSet<String>, &'static str> {
     for element in document.select(&selector) {
         if let Some(href) = element.value().attr("href") {
             if let Ok(url) = Url::parse(href) {
-                println!("{}", href);
                 urls.insert(href.to_string());
             }
         }
